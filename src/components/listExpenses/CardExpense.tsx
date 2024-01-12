bcadf3890cefefda3345ff59b830d69cd91483c9
Pedro Ramirez <prlara2000@gmail.com>
2024-01-11 23:29:40 -0600

    fix font size

--- src/components/listExpenses/CardExpense.tsx
+++ src/components/listExpenses/CardExpense.tsx
@@ -28,24 +28,24 @@ function CardExpense({
       <div className='m-0 p-0 col-12 d-flex flex-wrap justify-content-between'>
         {expenses.map((e) => {
           return (
-            <div key={e.id} className='col-12 col-sm-6 col-md-4 col-lg-3 p-1'>
-              <div className='card shadow bg-body rounded'>
-                <div className='card-body d-flex flex-wrap'>
+            <div key={e.id} className='col-12 col-sm-6 col-md-4 col-xl-3 p-1'>
+              <div className='card shadow bg-body rounded' id='card'>
+                <div className='card-body d-flex flex-wrap p-2' id='card-body'>
                   <div className='col-6 mb-3'>
-                    <h6 className='card-title text-muted m-0'>Description</h6>
+                    <p className='card-title text-muted m-0'>Description</p>
                     <p className='card-text fw-bold'>{e.description}</p>
                   </div>
                   <div className='col-6'>
-                    <h6 className='card-title text-muted m-0 text-end'>Amount</h6>
+                    <p className='card-title text-muted m-0 text-end'>Amount</p>
                     <p className='card-text text-end fw-bold'>${e.amount}</p>
                   </div>
 
                   <div className='col-6'>
-                    <h6 className='card-title text-muted m-0 '>Category</h6>
+                    <p className='card-title text-muted m-0 '>Category</p>
                     <p className='card-text fw-bold'>{e.category}</p>
                   </div>
                   <div className='col-6'>
-                    <h6 className='card-title text-muted m-0 text-end'>Date</h6>
+                    <p className='card-title text-muted m-0 text-end'>Date</p>
                     <p className='fw-bold text-end'>{e.date}</p>
                   </div>
 
